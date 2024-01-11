c8a6667b551424df58e455d5d0199cb7d12660c8
felix-ha <hauerf98@gmail.com>
2024-01-11 23:07:49 +0100

    Avoid missing file

--- frontend/logic.py
+++ frontend/logic.py
@@ -1,14 +1,20 @@
 import json
 from pathlib import Path
 from config import Config
+from datetime import datetime
+import logging
 
 
 def get_server_data(config: Config):
     state_file = Path(config.resources_path, config.state_file)
 
-    with open(state_file, 'r') as f:
-        json_data = json.load(f)
-    
+    try:
+        with open(state_file, 'r') as f:
+            json_data = json.load(f)
+    except Exception as e:
+        logging.exception(e)
+        json_data = {'update_time': datetime.now(), 'text': "failed"}
+        
     return json_data
 
 if __name__ == '__main__':
