97566b7f1c014fd4d0ca9c642da2c7f2ea98fba1
felix-ha <hauerf98@gmail.com>
2024-01-13 18:17:16 +0100

    Add tables

--- frontend/server.py
+++ frontend/server.py
@@ -1,21 +1,43 @@
-from fastapi import FastAPI, Request, Depends
+from datetime import datetime
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from logic import get_server_data
 from config import Config
+from pymongo import MongoClient
+
+
+try:
+    client = MongoClient('mongodb', 27017, username='root', password='example')
+    db = client["database"]
+    collection = db['states']
+except Exception as e:
+    logging.exception(f"connection to mongodb failed: {str(e)}")
+
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="frontend/templates")
 import logging
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     json_data = get_server_data(Config())
     return templates.TemplateResponse("index.html", {"request": request, "json_data": json_data})
+
+@app.get("/table", response_class=HTMLResponse)
+async def read_item(request: Request):
+    try:    
+        records = []
+        for document in collection.find():
+            records.append({"Date": document["update_time"], "text": document["text"]})
+    except Exception as e:
+        logging.info(f"retrieving entries from mongodb failed: {e}")
+        records = [{'update_time': datetime.now(), 'text': 'loading failed'}]
+    return templates.TemplateResponse("table.html", {"request": request, "records": records})
 
 
 if __name__ == "__main__":
