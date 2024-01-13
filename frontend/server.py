48536057a16d63bc27999a242f93fcd1790cbc50
felix-ha <hauerf98@gmail.com>
2024-01-13 18:58:12 +0100

    Reverse order of table

--- frontend/server.py
+++ frontend/server.py
@@ -34,6 +34,7 @@ async def read_item(request: Request):
         records = []
         for document in collection.find():
             records.append({"update_time": document["update_time"], "text": document["text"]})
+        records = records[::-1]
     except Exception as e:
         logging.info(f"retrieving entries from mongodb failed: {e}")
         records = [{'update_time': datetime.now(), 'text': 'loading failed'}]
